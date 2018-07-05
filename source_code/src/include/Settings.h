15119777edd07f4fe3932ed2f942dae15770b3b1
Dimitar Stanev <jimstanev@gmail.com>
2018-07-05 11:58:31 +0300

    working on documentation

--- source_code/src/include/Settings.h
+++ source_code/src/include/Settings.h
@@ -3,6 +3,6 @@
 
 #include <string>
 
-std::string MODEL_DIR = "D:/Desktop/upat_eye_model/source_code/../model/";
+std::string MODEL_DIR = "/home/mitkof6/dev/upat_eye_model/source_code/../model/";
 
 #endif
