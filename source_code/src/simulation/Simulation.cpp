49593ca7bea1c385953319147afee5bed30c8f55
Dimitar Stanev <jimstanev@gmail.com>
2018-07-09 12:10:48 +0300

    correct passive force parameters

--- source_code/src/simulation/Simulation.cpp
+++ source_code/src/simulation/Simulation.cpp
@@ -28,34 +28,34 @@ void addExpressionCoordinateForce(Model* model) {
     // remove other forces first
     model->updForceSet().remove(
         model->updForceSet().getIndex(
-            &model->updForceSet().get("add_adb_tissue"), 0));
+        &model->updForceSet().get("add_adb_tissue"), 0));
 
     model->updForceSet().remove(
         model->updForceSet().getIndex(
-            &model->updForceSet().get("sup_inf_tissue"), 0));
+        &model->updForceSet().get("sup_inf_tissue"), 0));
 
     model->updForceSet().remove(
         model->updForceSet().getIndex(
-            &model->updForceSet().get("inc_exc_tissue"), 0));
+        &model->updForceSet().get("inc_exc_tissue"), 0));
 
     // Add expression based coordinate forces (don't use spaces in the
     // expression). Linux users may experience problem with Lepton (v3.3), most
     // recent versions may not have this problem.
     auto add_adb_tissue =
         new ExpressionBasedCoordinateForce("r_eye_add_abd",
-                                           "-0.002225*q-0.0105*0.0001*q^3-0.001*qdot");
+                                           "-0.002225*q-34.5297*0.0001*q^3-0.002*qdot");
     add_adb_tissue->setName("add_adb_tissue");
     model->addForce(add_adb_tissue);
 
     auto sup_inf_tissue =
         new ExpressionBasedCoordinateForce("r_eye_sup_inf",
-                                           "-0.002225*q-0.0105*0.0001*q^3-0.001*qdot");
+                                           "-0.002225*q-34.5297*0.0001*q^3-0.002*qdot");
     sup_inf_tissue->setName("sup_inf_tissue");
     model->addForce(sup_inf_tissue);
 
     auto inc_exc_tissue =
         new ExpressionBasedCoordinateForce("r_eye_inc_exc",
-                                           "-0.002225*q-0.0105*0.0001*q^3-0.001*qdot");
+                                           "-0.002225*q-34.5297*0.0001*q^3-0.002*qdot");
     inc_exc_tissue->setName("inc_exc_tissue");
     model->addForce(inc_exc_tissue);
 }
@@ -89,12 +89,12 @@ void simulateModel() {
     /*model.updCoordinateSet().get("r_eye_add_abd").setValue(state, 0.1);
       model.updCoordinateSet().get("r_eye_sup_inf").setValue(state, 0.1);*/
 
-    // Visualize model
-    /*model.updMatterSubsystem().setShowDefaultGeometry(true);
-      model.updVisualizer().updSimbodyVisualizer().setBackgroundColor(Vec3(1));
-      model.updVisualizer().updSimbodyVisualizer().drawFrameNow(state);*/
+      // Visualize model
+      /*model.updMatterSubsystem().setShowDefaultGeometry(true);
+        model.updVisualizer().updSimbodyVisualizer().setBackgroundColor(Vec3(1));
+        model.updVisualizer().updSimbodyVisualizer().drawFrameNow(state);*/
 
-    // Create the integrator and manager for the simulation
+        // Create the integrator and manager for the simulation
     RungeKuttaMersonIntegrator integrator(model.getMultibodySystem());
     Manager manager(model, integrator);
 
